6db74233d8959d132f43514a9de12b4a4fe2397e
shivrajinnovative <shivraj.innovative@gmail.com>
2024-07-04 12:14:38 +0530

    navbar resposive set in all pages

--- script.js
+++ script.js
@@ -1,15 +1,13 @@
 window.onscroll = function() {
     var navbar = document.querySelector(".navbar");
     var scrollThreshold = window.innerWidth < 500 ? 80 : 100;
-    var leftMove = window.innerWidth < 500 ? '0' : '6%';
-    var topMove = window.innerWidth < 500 ? '90px' : '130px';
 
     if (window.pageYOffset >= scrollThreshold) {
         navbar.style.position = "fixed";
-        navbar.style.top = "0"; 
-        navbar.style.left = leftMove; 
+      
+  
     } else {
         navbar.style.position = "absolute";
-        navbar.style.top = topMove;
+        navbar.style.top = 0;
     }
 };
